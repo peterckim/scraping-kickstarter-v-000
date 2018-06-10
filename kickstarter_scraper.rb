8383916789eef389e1cbe3c8329687978ebe7030
Peter Kim <peterckim96@gmail.com>
2018-06-10 16:15:59 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -21,21 +21,7 @@ def create_project_hash
     }
     
   end
-  
   projects
-  
-  #projects: kickstarter.css("li.project.grid_4")
-  
-  #title: project.css("h2.bbcard_name strong a").text
-  
-  #image link: project.css("div.project-thumbnail a img").attribute("src")
-  
-  #description: project.css("p.bbcard_blurb").text
-  
-  #location: project.css("ul.project-meta span.location-name").text
-  
-  #percent_funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
-  binding.pry
 end
 
 create_project_hash
